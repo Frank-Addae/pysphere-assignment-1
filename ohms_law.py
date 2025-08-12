5ae40ce211acd3cf832bc2cb2c3677c8831682b4
Frank Addae <scinanasci97@gmail.com>
2025-08-12 02:03:16 +0000

    Added Ohm's Law assignment solution

---
+++ ohms_law.py
@@ -0,0 +1,11 @@
+# Ohm's Law Assignment 1 - by Frank Addae
+
+# Ask the user for input
+I = float(input("Enter the current (in A): "))
+R = float(input("Enter the resistance (in ohms): "))
+
+# Calculate voltage
+V = I * R
+
+# Display the result
+print(f"The voltage is: {V} volts")
